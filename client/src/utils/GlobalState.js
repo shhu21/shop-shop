0dbb990e89291dcd90cfa878a3604fa4b10d640e
shhu21 <hu.sara21@gmail.com>
2020-10-13 14:15:30 -0700

    adds cart functionalities

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -5,14 +5,16 @@ const StoreContext = createContext();
 const { Provider } = StoreContext;
 
 const StoreProvider = ({ value = [], ...props }) => {
-    const [state, dispatch] = useProductReducer({
-      products: [],
-      categories: [],
-      currentCategory: '',
-    });
-    // use this to confirm it works!
-    console.log(state);
-    return <Provider value={[state, dispatch]} {...props} />;
+  const [state, dispatch] = useProductReducer({
+    products: [],
+    cart: [],
+    cartOpen: false,
+    categories: [],
+    currentCategory: ''
+  });
+  // use this to confirm it works!
+  console.log(state);
+  return <Provider value={[state, dispatch]} {...props} />;
 };
 
 const useStoreContext = () => {
