c8316c7fee0b959dd2754952165a0cf5a8e707c1
shhu21 <hu.sara21@gmail.com>
2020-10-13 18:18:59 -0700

    adds success on checkout

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -5,9 +5,15 @@ import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import './style.css';
 import { useStoreContext } from '../../utils/GlobalState';
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/react-hooks';
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
     const [state, dispatch] = useStoreContext();
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
     useEffect(() => {
         async function getCart() {
@@ -20,6 +26,14 @@ const Cart = () => {
         }
     }, [state.cart.length, dispatch]);
 
+    useEffect(() => {
+        if (data) {
+          stripePromise.then((res) => {
+            res.redirectToCheckout({ sessionId: data.checkout.session });
+          });
+        }
+    }, [data]);
+
     function toggleCart() {
         dispatch({ type: TOGGLE_CART });
     }
@@ -30,6 +44,20 @@ const Cart = () => {
           sum += item.price * item.purchaseQuantity;
         });
         return sum.toFixed(2);
+    }
+
+    function submitCheckout() {
+        const productIds = [];
+
+        getCheckout({
+            variables: { products: productIds }
+        });
+      
+        state.cart.forEach((item) => {
+          for (let i = 0; i < item.purchaseQuantity; i++) {
+            productIds.push(item._id);
+          }
+        });
     }
 
     if (!state.cartOpen) {
@@ -55,8 +83,8 @@ const Cart = () => {
                 <strong>Total: ${calculateTotal()}</strong>
                 {
                 Auth.loggedIn() ?
-                    <button>
-                    Checkout
+                    <button onClick={submitCheckout}>
+                        Checkout
                     </button>
                     :
                     <span>(log in to check out)</span>
